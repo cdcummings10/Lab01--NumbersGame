2b1e556ffec78f21a6e7b9f030caad9fc577d3b8
cdcummings10 <ccummings0913@gmail.com>
2019-10-15 08:48:17 -0700

    Finished methods 1 and 2

---
+++ NumberGame/NumberGame/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StartSequence();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program has finished running");
+            }
+        }
+
+        static void StartSequence()
+        {
+            Console.WriteLine("Enter a number greater than 0.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int[] userArr = new int[userInput];
+            int[] fullArr = Populate(userArr);
+            int sum = GetSum(fullArr);
+            int product = GetProduct(fullArr, sum);
+            Console.ReadLine();
+        }
+
+        static int[] Populate(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Please enter a number {i + 1}/{arr.Length}");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            return arr;
+        }
+
+        static int GetSum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+        static int GetProduct(int[] arr, int sum)
+        {
+            try
+            {
+            Console.WriteLine($"Please enter a number between 1 and {arr.Length}");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int product = arr[userInput] * sum;
+            return product;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        static void GetQuotient() { }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Lab01--NumbersGame
+# Console Application README Example
+
+## About Me Quiz
+
+Lab01-Exception-Handling
+
+*Author: Chris Cummings*
+
+----
+
+## Description
+***[Tell me about your application, should be about a paragraph]***
+Create a math game that takes user input, manipulates data, and utilizes error handling, by following the given the specifications.
+
+---
+
+### Getting Started
+Clone this repository to your local machine.
+
+```
+$ git clone https://github.com/cdcummings10/Lab01--NumbersGame
+```
+
+### To run the program from Visual Studio:
+Select ```File``` -> ```Open``` -> ```Project/Solution```
+
+Next navigate to the location you cloned the Repository.
+
+Double click on the ```Lab01-About-Me``` directory.
+
+Then select and open ```AboutMe.sln```
+
+---
+
+### Visuals
+***[Add screenshots of your application in action]***
+
+#### Application Start
+![Image 1](https://via.placeholder.com/750x500)
+#### Using the Application
+![Image 1](https://via.placeholder.com/750x500)
+#### Application End
+![Image 1](https://via.placeholder.com/750x500)
+
+---
+
+### Change Log
+***[The change log will list any changes made to the code base. This includes any changes from TA/Instructor feedback]***  
+1.3: *Added summary comments to the methods* - 8 Nov 2010  
+1.2: *Changed variable names to follow proper convention* - 6 Nov 2010  
+1.1: *Added a Try/Catch/Finally for Question 2* - 5 Nov 2010  
+
+
+------------------------------
+For more information on Markdown: https://www.markdownguide.org/cheat-sheet
